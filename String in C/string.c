44dd5ef537974fb940865ed41f889c1b0f4fc512
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-23 11:37:07 +0600

    string file added

---
+++ String in C/string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+
+    char str[] = "Hello";
+    //char str[] = {'H','e','l','l','o'};
+    printf("%s\n", str);
+
+    char secondStr[20];
+    printf("\nEnter Your University Name: ");
+    //scanf("%s", &str2);
+    
+    printf("Your University Name is: %s\n", secondStr);
+
+}
