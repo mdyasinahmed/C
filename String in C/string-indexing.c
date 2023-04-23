40c7781988bbdf73c6449ef7878135f3b91fc470
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-23 11:48:17 +0600

    String Indexing code added

---
+++ String in C/string-indexing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // String Indexing  -- stringName[INDEX]
+    char str[] = "Bangladesh";
+
+    printf("%c\n", str[0]);
+    printf("%c\n", str[1]);
+    printf("%c\n", str[2]);
+    printf("%c\n", str[3]);
+    printf("%c\n", str[4]);
+
+    return 0;
+}
--- String in C/string.c
+++ String in C/string.c
@@ -2,15 +2,22 @@
 
 int main(){
 
-
+    // String Format
     char str[] = "Hello";
     //char str[] = {'H','e','l','l','o'};
     printf("%s\n", str);
 
-    char secondStr[20];
+
+    // String Input/Output
+    char secondStr[25];
     printf("\nEnter Your University Name: ");
     //scanf("%s", &str2);
-    
+    fgets(secondStr, sizeof(secondStr), stdin);
+
     printf("Your University Name is: %s\n", secondStr);
+
+    
+
+
 
 }
