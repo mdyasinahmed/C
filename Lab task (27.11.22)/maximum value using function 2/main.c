c711ad5128abc32640517a7dac060a6422f6f4b5
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-24 11:47:57 +0600

    repository folder merged

--- Lab task (27.11.22)/maximum value using function 2/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(number[5])
-{
-    for(int i=0; i)
-
-    return 0;
-}
--- Ph-Problem/print-it/print-it.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-   printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.\n");
-
-   return 0;
-}
